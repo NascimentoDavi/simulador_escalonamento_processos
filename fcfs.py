15fee72d17e4f9f2314e9edee3a855b0284645b7
Davi Rodrigues <davirnascimentox@gmail.com>
2025-06-11 21:12:55 -0300

    feat: add new enter data files | change file reading within main.py

--- fcfs.py
+++ fcfs.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 
+# Implementação do algoritmo First-Come - First Served - Um dos algoritmos mais simples utilizados em sistemas operacionas para gerenciar a ordem da execução de processos
+
 # FCFS - First-Come First-Served
 def escalonar_fcfs(processos):
     processos.sort(key=lambda p: p['arrival'])  # ordena com base na hora de chegada
@@ -7,22 +9,35 @@ def escalonar_fcfs(processos):
     gantt = []  # armazena as informações de tempo de execução de cada processo
 
     print('FCFS - Escalonamento')
+
+    # < : alinhamento a esquerda
+    # 10: ocupar 10 caracteres
     print(f"{'Processo':<10}{'Início':<10}{'Fim':<10}{'Espera':<10}{'Turnaround'}")
 
     for processo in processos:
-        inicio = max(tempo_atual, processo['arrival'])
-        fim = inicio + processo['burst']
+
+        # Processo só tem início quando o processador está livre (tempo_atual) e quando o próximo processo chega (processo['arrival']). Max() aciona o valor máximo, maior, para garantir que comece apenas qundo o processador está livre.
+        inicio = max(tempo_atual, processo['arrival']) 
+        
+        fim = inicio + processo['burst'] # Burst é o tempo de execução necessário, ou seja, o "tempo de CPU".
+
         espera = inicio - processo['arrival']
-        turnaround = fim - processo['arrival']
+
+        turnaround = fim - processo['arrival'] # Tempo total que processo leva, desde que chegou até terminar sua execução
 
         gantt.append((processo['id'], inicio, fim))
+        
         tempo_atual = fim
 
+        # < : alinhamento à esquerda
+        # 10: ocupa 10 caracteres
         print(f"{processo['id']:<10}{inicio:<10}{fim:<10}{espera:<10}{turnaround}")
 
     desenhar_gantt(gantt)
 
 def desenhar_gantt(gantt):
+    import matplotlib.colors as mcolors
+
     fig, gnt = plt.subplots()
     gnt.set_title("Gráfico de Gantt - FCFS")
     gnt.set_xlabel("Tempo")
@@ -32,16 +47,10 @@ def desenhar_gantt(gantt):
     gnt.set_yticklabels([processo[0] for processo in gantt])
     gnt.set_ylim(0, 10 + len(gantt)*20)
 
+    cores = list(mcolors.TABLEAU_COLORS.values())
+
     for i, (pid, inicio, fim) in enumerate(gantt):
-        gnt.broken_barh([(inicio, fim - inicio)], (10 + i*10, 9), facecolors=('tab:blue'))
+        cor = cores[i % len(cores)]
+        gnt.broken_barh([(inicio, fim - inicio)], (10 + i*10, 9), facecolors=(cor))
 
     plt.show()
-
-
-# # ERRO COMUM:
-# qt.glx: qglx_findConfig: Failed to finding matching FBConfig for QSurfaceFormat(version 2.0, options QFlags<QSurfaceFormat::FormatOption>(), depthBufferSize -1, redBufferSize 1, greenBufferSize 1, blueBufferSize 1, alphaBufferSize -1, stencilBufferSize -1, samples -1, swapBehavior QSurfaceFormat::SingleBuffer, swapInterval 1, colorSpace QSurfaceFormat::DefaultColorSpace, profile  QSurfaceFormat::NoProfile)
-# No XVisualInfo for format QSurfaceFormat(version 2.0, options QFlags<QSurfaceFormat::FormatOption>(), depthBufferSize -1, redBufferSize 1, greenBufferSize 1, blueBufferSize 1, alphaBufferSize -1, stencilBufferSize -1, samples -1, swapBehavior QSurfaceFormat::SingleBuffer, swapInterval 1, colorSpace QSurfaceFormat::DefaultColorSpace, profile  QSurfaceFormat::NoProfile)
-# Falling back to using screens root_visual
-
-# Este erro ocorre quando o QT não encontra a configuração gráfica ideal para usar o OpenGL na máquina.
-# Matplotlib usa um backend em QT(Qt5Agg ou Qt6Agg) e o QT chama OpenGL
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 from fcfs import escalonar_fcfs
 from sjf_nao_preemptivo import escalonar_sjf_naopreemptivo
 
+# Lê arquivos com processos de entrada
 def ler_entrada(arquivo):
     with open(arquivo, 'r') as f:
         linhas = f.readlines()
@@ -21,6 +23,19 @@ def ler_entrada(arquivo):
     return processos
 
 if __name__ == '__main__':
-    processos = ler_entrada("entrada.txt")
-    # escalonar_fcfs(processos)
-    escalonar_sjf_naopreemptivo(processos)
+
+    # Pasta com informacoes de entrada
+    pasta = "entradas"
+
+    # Arquivos da pasta
+    arquivos = [f for f in os.listdir(pasta) if os.path.isfile(os.path.join(pasta, f))]
+
+    if not arquivos:
+        print("Nenhum arquivo encontrado na pasta")
+    else:
+        for nome_arquivo in arquivos:
+            caminho_arquivo = os.path.join(pasta, nome_arquivo)
+            print(f"/n--- Executando {nome_arquivo} ---")
+
+            processos = ler_entrada(caminho_arquivo)
+            escalonar_fcfs(processos)
